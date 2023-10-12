b94724f37b1c3d92af4f7ddbb530d933b9eb5856
Abilash-devops <abilashdevopsaws@gmail.com>
2023-10-12 14:49:47 +0530

    commited

---
+++ roboshop-infra-standard/08-redis/redis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/centos
+git clone https://github.com/Abilash-devops/roboshell_terraform.git
+cd roboshell_terraform
+sh redis.sh
---
+++ roboshop-infra-standard/10-cart/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.15.0"
+    }
+  }
+
+  backend "s3" {
+    bucket   = "roboshop-terraform-padmasrikanth"
+    key = "cart-1a"
+    region = "us-east-1"
+    dynamodb_table = "roboshop"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  # you can give access key and secret key here, but security problem
+  region = "us-east-1"
+}
